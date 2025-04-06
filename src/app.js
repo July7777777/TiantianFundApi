3156408548cf1607385b8d892909897b44da5116
july <1114174347@qq.com>
2025-04-07 02:07:18 +0800

    新增 访问日志

--- src/app.js
+++ src/app.js
@@ -9,6 +9,28 @@ function startServe() {
 
     const router = new Router();
 
+    // 添加请求日志中间件
+    app.use(async (ctx, next) => {
+      // 记录请求开始时间
+      const start = Date.now();
+      // 打印请求方法和 URL
+      log(`Request Method: ${ctx.method}, Request URL: ${ctx.url}`);
+      // 打印请求头
+      log('Request Headers:', ctx.headers);
+      // 打印请求查询参数
+      log('Request Query:', ctx.query);
+      // 若为 POST、PUT 等包含请求体的方法，打印请求体
+      if (ctx.request.body) {
+        log('Request Body:', ctx.request.body);
+      }
+      // 继续处理后续中间件和路由
+      await next();
+      // 记录请求结束时间并计算耗时
+      const ms = Date.now() - start;
+      // 打印响应状态码和耗时
+      log(`Response Status: ${ctx.status}, Response Time: ${ms}ms`);
+    });
+
     getModules().forEach(({ fileName, path }) => {
       const routerPath = `/${fileName}`;
       const api = require(path);
