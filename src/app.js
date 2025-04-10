15e59aebd6e83767ef0c1a582b709961a58e0ed8
july <1114174347@qq.com>
2025-04-10 21:31:41 +0800

    qwq

--- src/app.js
+++ src/app.js
@@ -11,24 +11,12 @@ function startServe() {
 
     // 添加请求日志中间件
     app.use(async (ctx, next) => {
-      // 记录请求开始时间
       const start = Date.now();
-      // 打印请求方法和 URL
-      log(`Request Method: ${ctx.method}, Request URL: ${ctx.url}`);
-      // 打印请求头
-      log('Request Headers:', ctx.headers);
-      // 打印请求查询参数
-      log('Request Query:', ctx.query);
-      // 若为 POST、PUT 等包含请求体的方法，打印请求体
-      if (ctx.request.body) {
-        log('Request Body:', ctx.request.body);
-      }
-      // 继续处理后续中间件和路由
+
       await next();
-      // 记录请求结束时间并计算耗时
       const ms = Date.now() - start;
       // 打印响应状态码和耗时
-      log(`Response Status: ${ctx.status}, Response Time: ${ms}ms`);
+      log(`${ctx.method} ${ctx.status} ${ctx.url} ${ms}ms`);
     });
 
     getModules().forEach(({ fileName, path }) => {
@@ -40,7 +28,7 @@ function startServe() {
       log(`✅ 生成路由 ${routerPath}`);
 
       router.get(routerPath, async (ctx, next) => {
-        log(`📥  ${ctx.request} ${ctx.request.url}`);
+        // log(`📥  ${ctx.request} ${ctx.request.url}`);
         ctx.status = 200;
         ctx.body = await api(ctx.request.query, ctx);
         next();
