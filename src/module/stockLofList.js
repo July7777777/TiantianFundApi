08c08fc7370e5aebb5a438e2278437133e7b2079
July <1114174347@qq.com>
2025-04-27 00:57:26 +0800

    两个 LOF接口测试完成

--- src/module/stockLofList.js
+++ src/module/stockLofList.js
@@ -1,24 +1,22 @@
-const { request,fetch } = require('../utils/index.js');
+const { request,get } = require('../utils/index.js');
 
 /**
  * 股票LOF 列表
  * https://www.jisilu.cn/data/lof/stock_lof_list/?___jsl=LST___t=1745568051745&only_owned=&rp=25
- * get请求 暂未测试
+ * get请求 已测试
  * 参数
  * ___jsl: LST___t=1745567060341  时间戳 非必须
  * rp: 25 每页显示条数  默认25 非必须
- * only_owned: 是否只显示持有  默认false 非必须 日后得研究登陆了
+ * only_owned: 是否只显示持有  默认false？0？ 非必须 日后得研究登陆了
  * 最好不要请求太快 官方会员 才30s一次   但是手动连续快速请求也没事qwq
  */
 module.exports = async (params) => {
-  const url = 'https://www.jisilu.cn/data/lof/stock_lof_list';
-  const data =  fetch(url, {
+  const url = 'https://www.jisilu.cn/data/lof/stock_lof_list/';
+
+  return await request(url, {
     ___jsl: `LST___t=${Date.now()}`,
+    only_owned: '',
     rp: 25,
-    only_owned: false,
-    page:1,
     ...params,
   });
-  console.log(data);
-  return data;
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,8 +2,7 @@ const axios = require('axios');
 const glob = require('glob');
 
 const headers = {
-  validmark:
-    'aKVEnBbJF9Nip2Wjf4de/fSvA8W3X3iB4L6vT0Y5cxvZbEfEm17udZKUD2qy37dLRY3bzzHLDv+up/Yn3OTo5Q==',
+  validmark: 'aKVEnBbJF9Nip2Wjf4de/fSvA8W3X3iB4L6vT0Y5cxvZbEfEm17udZKUD2qy37dLRY3bzzHLDv+up/Yn3OTo5Q==',
 };
 
 const deviceId = '874C427C-7C24-4980-A835-66FD40B67605';
@@ -41,7 +40,7 @@ const post = async (url, data) => {
     }),
     {
       headers,
-    }
+    },
   );
   return res.data;
 };
@@ -55,7 +54,7 @@ const jsonp = async (url, callback, params) => {
 
 const getModules = () => {
   const files = glob.sync('./src/module/*.js');
-  return files.map((path) => {
+  return files.map(path => {
     const fileName = path.replaceAll('\\', '/').replace('src/module/', '').replace('.js', '');
     return {
       fileName,
@@ -76,24 +75,13 @@ const sse = async (url, params) => {
   return res.data;
 };
 
-const fetch =  (url, params) => {
-  const res =  axios.get(url, {
-    headers: {
-      "accept": "application/json, text/javascript, */*; q=0.01",
-      "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-      "priority": "u=1, i",
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "x-requested-with": "XMLHttpRequest",
-      "cookie": "kbzw__Session=hcf45k1grm605ogiqanq6qeg12; Path=/;",
-      "Referer": "https://www.jisilu.cn/data/lof/",
-      "Referrer-Policy": "strict-origin-when-cross-origin"
-    },
-    params // 如果有查询参数，则通过 params 传入
-  });
-  return res.data;
-}
+// const get = async (url, params) => {
+//   const res = await axios.get(url, {
+//     headers,
+//     params, // 如果有查询参数，则通过 params 传入
+//   });
+//   return res.data;
+// };
 
 module.exports = {
   request,
@@ -101,5 +89,5 @@ module.exports = {
   jsonp,
   getModules,
   sse,
-  fetch
+  // get,
 };
